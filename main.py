23fbf383f0eb483ed46bdd53687f1c923e7ac4d3
FlavioRyu <flavioryu@gmail.com>
2026-07-13 23:14:29 +0100

    Add .gitignore and feature to handle recruiting company postings

--- main.py
+++ main.py
@@ -12,9 +12,13 @@ def main():
         if user_input.lower() == "exit":
             print("Keep fighting! ✊")
             break
+        yes_no = input("Is this from a recruiting company? yes/no: ")
+        is_recruiter = True if yes_no.lower() == "yes" else False
 
         linkedin_scraper = LinkedinScraper(user_input)
         company_name = linkedin_scraper.get_company_name()
+        if is_recruiter:
+            company_name = f"N/A [{company_name}]"
         location = linkedin_scraper.get_location()
         position_name = linkedin_scraper.get_position_name()
 
@@ -22,7 +26,14 @@ def main():
         date = date_calculator.get_date()
 
         sheet_manager = SheetManager()
-        result = sheet_manager.add_row(company_name, location, position_name, date, user_input, linkedin_scraper.site)
+        result = sheet_manager.add_row(
+            company_name,
+            location,
+            position_name,
+            date,
+            user_input,
+            linkedin_scraper.site
+        )
 
         pprint(result)
         print("")
